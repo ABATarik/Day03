600608a46f9abdfa2f30654c2960dbb96ffce7c0
ABATarik <abaspahic.tarik@outlook.com>
2021-03-10 23:48:29 +0100

    Case Sensitivity

---
+++ ex07/src/index.js
@@ -0,0 +1,26 @@
+function main() {
+    
+    
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver =9000;
+   
+   
+    return{
+    studlyCapVar,
+    properCamelCase,
+    titleCaseOver
+
+
+    }
+    
+}
+
+main();
+
+module.exports = main;
