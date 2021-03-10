93682a53b9654136515dced99e70c0d524c4c3fd
ABATarik <abaspahic.tarik@outlook.com>
2021-03-10 22:39:38 +0100

    Javascript commenting

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+
+    //This is single line javascript comment
+
+    /* This is multi line javascript comment
+       This is multi line javascript comment
+       This is multi line javascript comment
+        */
+    }
+    
+    main();
+    
+    module.exports = main;
